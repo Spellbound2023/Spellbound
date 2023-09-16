991772cc1934e154cd5f923fcddee923e91934b6
Gowrienanthan  Balarupan <gowrie@promiseq.com>
2023-09-17 00:44:54 +1000

    Create the Home (landing) page.

---
+++ app/classic/GameBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WordInfo from './WordInfo';
+
+const GameBox = () => {
+    return (
+        <div>
+            <WordInfo/>
+        </div>
+    );
+};
+
+export default GameBox;
---
+++ app/classic/WordInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../../styles/classic/WordInfo.module.css'
+
+const WordInfo = () => {
+    return (
+        <div className={styles.container}>
+            {/* <h6 className={styles.heading}>Definition</h6>
+            <p className={styles.definition}>A medical term describing inflammation of the vascular layer of the eye, which includes the iris and ciliary body.</p> */}
+        </div>
+    );
+};
+
+export default WordInfo;
