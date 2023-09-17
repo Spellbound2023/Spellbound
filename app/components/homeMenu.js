3ffa761e29ff42346fefa1e3040f98cb22344c5a
Liam Locher <locherliam@gmail.com>
2023-09-17 21:23:02 +1000

    added icons to buttons

--- app/components/homeMenu.js
+++ app/components/homeMenu.js
@@ -1,34 +1,59 @@
 'use client';
 import React from 'react'
 import styles from './homeMenu.module.css'
+import Image from 'next/image'
 
 
 const MainMenu = () => {
   return (
     <div class={styles.MenuContainer}>
-          <a href="/classic">
-            <div class={styles.Button}>
-              <b>Classic</b>
-            </div>
-          </a>
+          <div class={styles.ButtonOuter}>
+            <a href="/classic">
+              <div class={styles.Button}>
+                <Image src='/Bookmark.png'
+                width={100}
+                height={100}/>
+                <br/>
+                <b>Classic</b>
+              </div>
+            </a>
+          </div>
         
-          <a href="/classic">
-            <div class={styles.Button}>
-              <b>Versus</b>
-            </div>
-          </a>
-        
-          <a href="/classic">
-            <div class={styles.Button}>
-              <b>Challenges</b>
-            </div>
-          </a>
+          <div class={styles.ButtonOuter}>
+            <a href="/classic">
+              <div class={styles.Button}>
+                <Image src='/Lightning Bolt.png'
+                width={100}
+                height={100}/>
+                <br/>
+                <b>Versus</b>
+              </div>
+            </a>
+          </div>
 
-          <a href="/classic">
-            <div class={styles.Button}>
-              <b>Statistics</b>
-            </div>
-          </a>
+          <div class={styles.ButtonOuter}>
+            <a href="/classic">
+              <div class={styles.Button}>  
+                <Image src='/Tightrope Walking.png'
+                width={100}
+                height={100}/>
+                <br/>
+                <b>Challenges</b>
+              </div>
+            </a>
+          </div>
+
+          <div class={styles.ButtonOuter}>
+            <a href="/classic">
+              <div class={styles.Button}>
+                <Image src='/Combo Chart.png'
+                width={100}
+                height={100}/>
+                <br/>
+                <b>Statistics</b>
+              </div>
+            </a>
+          </div>
       </div>
   )
 }
