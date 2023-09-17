09aed7f9bb11424601edf855b14d8313ab03acc8
Gowrienanthan  Balarupan <gowrie@promiseq.com>
2023-09-17 14:20:05 +1000

    Implement basic word information component with harcoded values

--- app/classic/GameBox.jsx
+++ app/classic/GameBox.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import WordInfo from './WordInfo';
+import React from "react";
+import WordInfo from "./WordInfo";
+import styles from "../../styles/classic/GameBox.module.css";
 
 const GameBox = () => {
-    return (
-        <div>
-            <WordInfo/>
-        </div>
-    );
+  return (
+      <div className={styles.mainContainer}>
+        <WordInfo definition="having or marked by great volume or bulk : large; also : full" />
+      </div>
+  );
 };
 
 export default GameBox;
--- app/classic/WordInfo.jsx
+++ app/classic/WordInfo.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import styles from '../../styles/classic/WordInfo.module.css'
+"use client";
 
-const WordInfo = () => {
-    return (
-        <div className={styles.container}>
-            {/* <h6 className={styles.heading}>Definition</h6>
-            <p className={styles.definition}>A medical term describing inflammation of the vascular layer of the eye, which includes the iris and ciliary body.</p> */}
-        </div>
-    );
+import React from "react";
+import styles from "../../styles/classic/WordInfo.module.css";
+
+const playAudio = (e) => {
+  e.preventDefault();
+  document.getElementById("pronounciation").play();
+};
+
+const WordInfo = ({ definition }) => {
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.defContainer}>
+        <h6 className={styles.heading}>Definition</h6>
+        <p className={styles.definition}>{definition}</p>
+      </div>
+      <div className={styles.audioContainer}>
+        <audio
+          id="pronounciation"
+          src="https://media.merriam-webster.com/audio/prons/en/us/mp3/v/volumi02.mp3"
+        ></audio>
+        <a onClick={playAudio}>
+          <object className={styles.soundIcon} data="/sound_icon.svg">
+            {" "}
+          </object>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default WordInfo;
