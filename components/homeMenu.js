9bab52618c54ffbcd18a6604bba9468d8f2eb4eb
Arkforest <arkforest@yahoo.com>
2023-10-18 22:58:54 +1000

    Removed challenges and metrics from home menu

--- components/homeMenu.js
+++ components/homeMenu.js
@@ -26,30 +26,6 @@ const MainMenu = () => {
           </div>
         </Link>
       </div>
-
-      <div className={styles.ButtonOuter}>
-        <Link href="/">
-          <div className={styles.Button}>
-            <Image
-              src="/images/Tightrope Walking.png"
-              width={100}
-              height={100}
-            />
-            <br />
-            Challenges
-          </div>
-        </Link>
-      </div>
-
-      <div className={styles.ButtonOuter}>
-        <Link href="/">
-          <div className={styles.Button}>
-            <Image src="/images/Combo Chart.png" width={100} height={100} />
-            <br />
-            Statistics
-          </div>
-        </Link>
-      </div>
     </div>
   );
 };
