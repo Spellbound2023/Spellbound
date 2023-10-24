064129a1e90785e239b27c378fbeeae9946f5c06
Arkforest <arkforest@yahoo.com>
2023-10-24 22:53:19 +1000

    Next API Documentation

--- app/api/randword/route 2.js
+++ app/api/randword/route 2.js
@@ -1,8 +1,11 @@
 import { getRandomWord, getWordDefAndAudio } from "@/utils/dictionaryAPI";
 import { NextResponse } from "next/server";
 
+/* GET request */
 export const GET = async (request) => {
+  // Asynchronously fetches random word 
   const word = await getRandomWord();
+  // Asynchronously fetches definition and audio for word 
   const wordData = await getWordDefAndAudio(word);
   return NextResponse.json(wordData);
 };
